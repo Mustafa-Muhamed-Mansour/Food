e5d52d49cf7afefa4153a0d04c15ed3d6fc63fb9
Mustafa-Muhamed-Mansour <mostafamansour163@yahoo.com>
2021-10-26 16:54:40 +0200

    first commit

---
+++ app/src/main/java/com/food/home/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.food.home;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.food.R;
+
+public class HomeFragment extends Fragment
+{
+
+
+    private HomeViewModel homeViewModel;
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+
+    }
+}
---
+++ app/src/main/java/com/food/repositories/HomeRepository.java
@@ -0,0 +1,5 @@
+package com.food.repositories;
+
+public class HomeRepository
+{
+}
